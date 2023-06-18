b37fc2d47f9b8a50cf7130bf5cabaf75ee97d498
Leave-it-blank <leaveitblank32@gmail.com>
2023-06-18 23:43:03 +0530

    added index

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,50 +1,10 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
-import { useState, useEffect } from 'react'
-const inter = Inter({ subsets: ['latin'] })
-
-type user = Array<{
-  id: string,
-  firstName: string,
-  lastName: string,
-  username: string,
-  email: string,
-  phone: string,
-  address: {
-    address: string,
-    city: string,
-    state: string
-  }
-  
-}>
+import Link from "next/link" 
 export default function Home() {
-  const [users, setUsers] = useState<user> ([])
-  console.log(users.length == 1)
-
-  useEffect(() =>{
-    const getUsers  = async () => {
-        const data = await fetch('https://dummyjson.com/users');
-        if(data.ok){
-          const users = await data.json();
-          setUsers(users.users)
-        }
-    }
-    getUsers();
-   
-  }, []);
-  console.log(users);
-
-  if(users.length == 0){
-    return <div className='container text-4xl mx-auto sm:p-10'>
-      <h1>Task 4</h1> 
-      <div>
-        <h2>Loading...</h2>
-      </div>
-      </div>
-  }
+ 
+ 
   return (
    <div className='container mx-auto py-5 my-5'>
-      <h1 className='text-4xl  font-bold p-1 sm:px-10'>Task 4</h1>
+      <h1 className='text-4xl  font-bold p-1 sm:px-10'>INDEX ALL TASK ETHANUS MERN</h1>
         <div className='p-2 sm:p-10'>
           <h2>Users Table</h2>
 
@@ -52,26 +12,33 @@ export default function Home() {
             <thead>
               <tr className=''>
               <th>ID</th>
-              <th>First Name</th>
-               <th>Last Name</th>
-               <th>username</th>
-               <th> email</th>
-                <th> phone</th>
-               <th>address</th>
+              <th>TASK</th>
+               <th>LINK</th>
                 </tr>
             </thead>
             <tbody>
-              {users.map((user) => (
-                <tr key={user.id}>
-                  <td>{user.id}</td>
-                  <td>{user.firstName}</td>
-                  <td>{user.lastName}</td>
-                  <td>{user.username}</td>
-                  <td>{user.email}</td>
-                  <td>{user.phone}</td>
-                  <td>{user.address.address +", " + user.address.city + ", " + user.address.state}</td>
+            <tr key={"1"}>
+                 <td>1</td>
+                 <td>Task 1</td>
+                 <td className="text-sky-500 hover:text-red-600"><Link href="/task-1">Link</Link></td>
                 </tr>
-              ))}
+             
+                <tr key={"4"}>
+                 <td>4</td>
+                 <td>Task 4</td>
+                 <td className="text-sky-500 hover:text-red-600"><Link href="/task-4">Link</Link></td>
+                </tr>
+                <tr key={"5"}>
+                 <td>5</td>
+                 <td>Task 5</td>
+                 <td className="text-sky-500 hover:text-red-600"><Link href="/task-4">Link</Link></td>
+                </tr>
+                <tr key={"6"}>
+                 <td>6</td>
+                 <td>Task 6</td>
+                 <td className="text-sky-500 hover:text-red-600"><a href="https://leaveitblank.co/">Link</a></td>
+                </tr>
+             
             </tbody>
           </table>
       </div>
